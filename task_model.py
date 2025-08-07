e1a917dc2bb84d55da48c5f5173e2b424905fbcc
Harlem Hernandez <harlemj-hernandezm@unilibre.edu.co>
2025-08-07 01:04:18 -0500

    Añadir set_done y remove_task en TaskModel

--- task_model.py
+++ task_model.py
@@ -2,14 +2,17 @@
 class TaskModel:
     def __init__(self, task_name):
         self.task_name = task_name
-        self.is_completed = False
+        self.is_done = False
 
     def get_task_name(self):
         return self.task_name
 
-    def delete_task(self):
+    def set_done(self):
+        self.is_done = True
+    
+    def remove_task(self):
         self.task_name = None
-        self.is_completed = False
-
-    def is_completed(self):
-        return self.is_completed
+        self.is_done = False
+    
+    def is_done(self):
+        return self.is_done
